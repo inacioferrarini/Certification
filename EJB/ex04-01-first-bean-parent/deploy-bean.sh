56ddd908302aa61c9d1ac98cc3ceed2b50d3f366
inacioferrarini <inacio.ferrarini@gmail.com>
2012-11-08 06:37:55 -0200

    uploading project

---
+++ EJB/ex04-01-first-bean-parent/deploy-bean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ex04-01-first-bean-ejb/target/ex04-01-first-bean-ejb.jar $JBOSS_HOME/server/default/deploy
---
+++ EJB/ex04-01-first-bean-parent/undeploy-bean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm $JBOSS_HOME/server/default/deploy/ex04-01-first-bean-ejb.jar
